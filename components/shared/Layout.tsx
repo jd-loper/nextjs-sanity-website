52bfe339a98d216057cb2b9f3b8942f504bec531
Joseph Loper <jd.loper@icloud.com>
2023-08-01 12:32:14 -0700

    Remove IntroTemplate component and cover image from Layout.tsx.

--- components/shared/Layout.tsx
+++ components/shared/Layout.tsx
@@ -2,7 +2,6 @@ import { Footer } from 'components/global/Footer'
 import { Navbar } from 'components/global/Navbar'
 import PreviewNavbar from 'components/global/PreviewNavbar'
 import { PreviewBanner } from 'components/preview/PreviewBanner'
-import IntroTemplate from 'intro-template'
 import { SettingsPayload } from 'types'
 
 const fallbackSettings: SettingsPayload = {
@@ -33,7 +32,6 @@ export default function Layout({
       )}
       <div className="mt-20 flex-grow px-4 md:px-16 lg:px-32">{children}</div>
       <Footer footer={settings?.footer} />
-      <IntroTemplate />
     </div>
   )
 }
